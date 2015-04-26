ab594b04c4f6053bcfb4451cc443c4c6584691bf
DaniBest <DaniBest@users.noreply.github.com>
2015-04-26 20:56:46 -0300

    Update Pratica32.java

--- src/main/java/Pratica32.java
+++ src/main/java/Pratica32.java
@@ -7,8 +7,18 @@
  * @author Daniel Anderson de Freitas
  */
 public class Pratica32 {
+
     public static double densidade(double x, double media, double desvio) {
-        double d = (1.0/(Math.sqrt(2.0*Math.PI)*desvio)*Math.exp(-(1.0/2.0)*Math.pow(((x-media)/desvio),2.0)));
+        double d = (1.0 / (Math.sqrt(2.0 * Math.PI) * desvio) * Math.exp(-(1.0 / 2.0) * Math.pow(((x - media) / desvio), 2.0)));
         return d;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Pratica32.densidade(-1.0, 67.0, 3.0));
+        if (Pratica32.densidade(-1.0, 67.0, 3.0) == (1.0 / (Math.sqrt(2.0 * Math.PI) * 3.0) * Math.exp(-(1.0 / 2.0) * Math.pow((((-1.0) - 67.0) / 3.0), 2.0)))) {
+            System.out.println("Valor Calculado Está Correto.");
+        }
+
     }
 }
